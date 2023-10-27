b51142c076ece0b35708575e427bfcc1bcacaaf1
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-10-26 21:07:24 -0500

    Initial commit

---
+++ src/test/java/co/com/asset/iotservice/IotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.asset.iotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
